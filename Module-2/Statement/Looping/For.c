674338298b083990ebb0af098f806948e3b37af3
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-10-19 09:34:07 +0530

    19-10-2023

---
+++ Module-2/Statement/Looping/For.c
@@ -0,0 +1,11 @@
+// WAP to print 80 to 70 using for loop
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i = 80; i >= 70; --i)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
---
+++ Module-2/Statement/Looping/While-Loop.c
@@ -0,0 +1,11 @@
+// WAP to print 1 to 10 using loop
+#include<stdio.h>
+int main()
+{
+    int i = 0;
+    while(i < 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+}
