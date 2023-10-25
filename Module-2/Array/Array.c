72b9c073f48cd2da9239191e247e6bb30eb5fe85
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-10-25 17:48:09 +0530

    25-10-2023

---
+++ Module-2/Array/Array.c
@@ -0,0 +1,11 @@
+// WAP to store 1 to 10 using array
+#include<stdio.h>
+int main()
+{
+    int array[5] = {5, 9, 3, 4, 2};
+
+    for(int i = 0; i < 5; i++){
+    printf("Value of array[%d]: %d", i, array[i]);
+    }
+    return 0;
+}
